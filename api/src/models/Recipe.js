af081eb3ae664a555d35fe4c7af985420fe16cfc
Federico Iglesia <federico.iglesia@outlook.com>
2022-07-16 16:34:02 -0300

    models creation

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,35 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define("recipe", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    dish: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    health_score: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    steps: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
     },
   });
 };
